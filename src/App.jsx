4ae5a39adc805148f82f777ce21f868c55169b0c
JD HINCAMAN <nverdolaga2003@gmail.com>
2021-11-30 21:31:23 -0500

    agregado el router-dom y las paginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import "./styles/styles.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Registro from "pages/Registro";
+import Login from "pages/Login";
+import Admin from "pages/Admin";
+import Index from "pages/Index";
 
 function App() {
-  return <div>Hola mundo</div>;
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/registro">
+            <Registro />
+          </Route>
+          <Route path="/admin">
+            <Admin />
+          </Route>
+          <Route path="/">
+            <Index />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
