aa71eb1dba15de26110e339968d38d17c58e5a44
JD HINCAMAN <nverdolaga2003@gmail.com>
2022-02-18 15:18:50 -0500

    pagina vehiculos actualizada

--- src/pages/admin/Vehiculos.jsx
+++ src/pages/admin/Vehiculos.jsx
@@ -9,41 +9,14 @@ const Vehiculos = () => {
     console.log("Hola Mundo");
   }, []);*/
 
-  const [nombreVehiculo, setNombreVehiculo] = useState("");
-
-  useEffect(() => {
-    console.log(
-      "Esta es una funcion que se ejecuta cada vez que nombre vehiculo cambie"
-    );
-    console.log("El valor de la variable es: ", nombreVehiculo);
-  }, [nombreVehiculo]);
-
-  const cambioDeNombre = (evento) => {
-    setNombreVehiculo(evento.target.value);
-  };
-
-  const enviarDatosAlBackend = () => {
-    console.log("el valor de la variable nombreVehiculo es: ", nombreVehiculo);
-  };
-
   return (
-    <form className="flex flex-col">
-      <h2>Formulario Registro de Vehiculos</h2>
-      <input
-        onChange={cambioDeNombre}
-        type="text"
-        placeholder="Nombre del vehiculo"
-      />
-      <input type="text" placeholder="Marca del vehiculo" />
-      <input type="text" placeholder="Modelo del vehiculo" />
-      <button
-        type="button"
-        onClick={enviarDatosAlBackend}
-        className="bg-yellow-300"
-      >
-        Enviar Datos
+    <div>
+      <h2>Pagina administracion de vehiculos</h2>
+      <button className="text-black bg-yellow-400 p-5 roun">
+        {" "}
+        Crear Nuevo Vehiculo
       </button>
-    </form>
+    </div>
   );
 };
 
